92ac86aec661f2d0684af6450eacf08436aba044
Yi Yan <yiyantay@gmail.com>
2015-11-06 10:51:52 -0500

    bugfix for server

--- index.js
+++ index.js
@@ -63,4 +63,9 @@ app.post('/api/remit',
     res.json(req.body); // requires bodyParser to work
   }
 );
-app.listen(3000);
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
